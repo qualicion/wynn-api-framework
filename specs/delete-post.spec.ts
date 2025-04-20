c669e132cc6cf3d47c3b1e6e35a5f04f6a78ab6b
Babatunde Oduniyi <qualicion@outlook.com>
2025-04-20 14:07:43 +0200

    Updated the test to cover the error scenarios for deleting a post

--- specs/delete-post.spec.ts
+++ specs/delete-post.spec.ts
@@ -1,11 +1,21 @@
 import mainController from "../controller/main.controller";
+import { mockDeletePost, postNotFoundMock } from "../data/delete-post.mock.data";
 
 describe("Delete post", () => {
-    it("should successfully delete a post and return empty response", async () => {
-      const deletedPostResponse = await mainController.deletePost("11");
+  it("should successfully delete a post and return empty response", async () => {
+    const deletedPostResponse = await mainController.deletePost("11");
 
-      expect(deletedPostResponse.status).toEqual(200);
+    expect(deletedPostResponse.status).toEqual(200);
 
-      expect(deletedPostResponse.body).toEqual({});
-    });
+    expect(deletedPostResponse.body).toEqual({});
   });
+
+  it("should return 404 when deleting non-existent post", async () => {
+    // Mocked only for this test
+    mockDeletePost(jest, postNotFoundMock);
+
+    const deletedPostResponse = await mainController.deletePost("999");
+    expect(deletedPostResponse.status).toEqual(404);
+    expect(deletedPostResponse.body.error).toEqual("Post not found");
+  });
+});
