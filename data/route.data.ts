2968dccc8479354da42ca774edba742cd0a9e91c
Babatunde Oduniyi <qualicion@outlook.com>
2025-04-20 13:21:56 +0200

    Test data for API requests

---
+++ data/route.data.ts
@@ -0,0 +1,15 @@
+export const postData = {
+  userId: 11,
+  title: "Wynn resort",
+  body: "This is the best integrated resort in the MENA region",
+};
+
+export const updateData = {
+  userId: 11,
+  title: "Updated Wynn resort",
+  body: "We are the only integrated resort in the MENA region",
+};
+
+export const updateSomeData = {
+  body: "We have the license to operate in the MENA region",
+};
